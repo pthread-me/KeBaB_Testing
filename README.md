b9779ed3deae0461e9bbb4857297de38b314afce
pthread-me <anas.n.alhadi@outlook.com>
2025-12-19 15:00:14 -0400

    Done, will start integrating it to the KeBaB src code now :)

--- README.md
+++ README.md
@@ -10,9 +10,9 @@ All the work is in src/main BTW
 
 ## Prelim:
 Probably not going to implement the SIMD version of blocked Bloom filter, why?
-    - It requires a complete refactor of the BloomFilter class in KeBaB which i dont have the auth to do nor want to rn
-    - IDK the size of the dataset its author tested it on
-    - Different Licence which idk how to deal with when copying
+-   It requires a complete refactor of the BloomFilter class in KeBaB which i dont have the auth to do nor want to rn
+-   IDK the size of the dataset its author tested it on
+-   Different Licence which idk how to deal with when copying
 
 ## Good news:
 I'm "designing" the register filter myself :P (Going great btw). 
@@ -24,8 +24,7 @@ Turns out GCC with -O3 optimized away the byte swapping using bswap and is more 
 There is a \_\_builtin\_bitreverse64 but im poor and my laptop doesnt have it ig :(
 
 On other bad news:
-I bombed an RBC Capital markets interview soooo bad i wanna kill myself sooooo bad, like
-i'll pay all my saving to be shot rn, maybe after KeBab :P 
+I bombed an RBC Capital markets interview soooo bad i just wanna die :(
 
 
 Okkk thats it, I'll hopefully finish this by christmas and will try to not bother nate with it till after. oh
--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 #include <vector>
 #include <tmmintrin.h>
 #include <bloom_filter.hpp>
-
-
+#include <random>
+#include <fstream>
 
 using namespace std;
 using word_t = uint64_t;
@@ -14,14 +14,43 @@ const word_t MAX_WORD = ULLONG_MAX;
 const word_t WORD_LEN = ULLONG_WIDTH;
 const __m64 INTEL_PSHUFB_FLAG = __m64(0x1020304050607); 
 
+
+
+
+namespace seeds{
+
+// Supports at most 32 hash functions
+constexpr uint64_t SEED[] = {          
+    0x153C67147CEBD9C1, 0xE9E9221977E2486E,
+    0xBD2A5DE364F86CEC, 0xF53E63242C7C96CA,
+    0xEA71F713607B8025, 0xDA1DC2E81860AC93,
+    0x700FC578B9B89EFC, 0x7ED09A9433D0F542,
+    0xED43BDEDBCF69432, 0x1D322B028A861DAA,
+    0x6E8CDB8F04EE5FFD, 0xEC53221EFD3A5C53,
+    0x01EE14F09892D967, 0xD6382ACCCBCF0420,
+    0xD448F78598D09FBE, 0x922AA2623D2BF77A,
+    0x4AF98D70BD02F4D9, 0xBE9A532696D539D9,
+    0x57CB1CF8FA6F105D, 0x4347990C105CF57C,
+    0xD5E6B9B31C51D5D6, 0x2196C4CF3D467371,
+    0x78BD99C62BA864CD, 0x0B747BD60B9F2FB4,
+    0xE636A63B15DC2C60, 0xE3D4C1379D7C2FF0,
+    0x2B5C7FAF45C1B370, 0xFE0247B305095328,
+    0xE4F3205AADABEA31, 0xD631A450CF4BA7BA,
+    0x7E0034EEC6C9E610, 0xCAF71C56BB5D4B4D
+};
+}
+
+
+
+
 template<typename Hash = kebab::MultiplyShift>
 void init(size_t elements, double error_rate, size_t num_hashes, kebab::MultiplyShift& hash, size_t& bits, size_t& set_bits, vector<word_t>& filter) {
-    bits = 256;
     set_bits = 0;
+    num_hashes = 3;
 
     // The filter should have a protected boundary word which can not be indexed to
     // but can be part of a block
-    filter = std::vector<word_t>(bits/WORD_LEN + WORD_LEN, 0ULL);
+    filter = std::vector<word_t>(elements/WORD_LEN + WORD_LEN, 0ULL);
   
     hash = Hash(bits); 
 }
@@ -100,25 +129,112 @@ size_t set_word(vector<word_t>& filter, size_t hash, word_t word){
 }
 
 
-int main(){
+bool contains(vector<word_t>& filter, size_t hash, size_t num_hashs){
+  size_t word = get_word(filter, hash);
+  for (auto i=0; i<num_hashs; ++i){
+    size_t pos = (hash * seeds::SEED[i]) % WORD_LEN;
+    if (!((word >> pos) & static_cast<word_t>(1))) return false; 
+  }
+  return true;
+}
 
+void add(vector<word_t>& filter, size_t hash, size_t num_hashs){
+  size_t b1 = hash/WORD_LEN; 
+  if(hash % WORD_LEN == 0){
+    size_t mask = 0;
+    for (size_t i=0; i<num_hashs; ++i){
+      size_t pos = (hash * seeds::SEED[i]) % WORD_LEN;
+      size_t cur = 1ULL<<pos;
+      mask |= cur;
+    }
+    println("Block: {}, Block before: {}", b1, filter[b1]);
+    filter[b1] |= mask;
+    println("mask: {}, block after: {}", mask, filter[b1]);
+
+
+  }else{
+    size_t start = hash%WORD_LEN;
+    size_t b2 = b1 + 1; 
+    size_t mask1 = 0, mask2 = 0;
+
+    for (size_t i=0; i<num_hashs; ++i){
+      size_t pos = (hash * seeds::SEED[i]) % WORD_LEN;
+      println("start: {}, pos: {}", start, pos);
+      if(start+pos < WORD_LEN){
+        println("Added to mask1 at {}", start+pos);
+        mask1 |= (1ULL<<(start+pos));
+        println("Mask1: {}", mask1);
+      }else{
+        println("Added to mask2 at {}", pos-(WORD_LEN-start));
+        mask2 |= (1ULL<<(pos-(WORD_LEN-start)));
+        println("Mask2: {}", mask2);
+      }
+    }
+    println("start: {}", start);
+    println("Block1: {}, Block1 before: {}", b1, filter[b1]);
+    println("Block2: {}, Block2 before: {}", b2, filter[b2]);
+    filter[b1] |= mask1;
+    filter[b2] |= mask2;
+
+    println("mask1: {}, block1 after: {}", mask1, filter[b1]);
+    println("mask2: {}, block2 after: {}", mask2, filter[b2]);
+    println("Final word: {}", get_word(filter, hash));
+  }
+}
+
+
+void check(){
+  std::random_device rd;
+  std::default_random_engine gen(rd());
+  std::uniform_int_distribution<uint64_t> dist(0, 99999);
+  std::uniform_int_distribution<uint64_t> dist2(0, 1);
+  
   size_t bits = 0;
   size_t set_bits = 0;
+  size_t num_hashs = 0;
   vector<word_t> filter;
 
   kebab::MultiplyShift hash;
-  init(300, 0.1, 3, hash, bits, set_bits, filter);
-
-  filter[0] = 0xa000000000000000;
-  filter[1] = 0xE000000000000003;
+  init(99999, 0.1, num_hashs, hash, bits, set_bits, filter);
+  
+  vector<size_t> hashes;
+  vector<size_t> checks;
   
 
-//  set_bits += set_word(filter, 66, MAX_WORD);
-  size_t w1 = get_word(filter, 61);
-  set_bits += set_word(filter, 61, 167);
-  size_t w2 = get_word(filter, 61);
-  println("{} + {} bits => {}", w1, set_bits, w2);
 
-//  uint64_t a = 4;
-//  reverse_bits(a);
+  for(size_t i=0; i<1000; ++i){
+    size_t val = dist(gen);
+    hashes.push_back(val);
+    add(filter, val, 3);
+  }
+
+  for(size_t i=0; i<1000; ++i){
+    if(dist2(gen)){
+      checks.push_back(hashes[i]);
+    }else{
+      checks.push_back(dist(gen));
+    }
+  }
+  
+ 
+  for(auto e: checks){
+    bool in_hashes = std::find(hashes.begin(), hashes.end(), e) != hashes.end();
+    bool in_filter = contains(filter, e, 3);
+
+    if(in_filter and in_hashes){
+      println("True positive: {}", e);
+    }else if( !in_filter and in_hashes){
+      println("False negative: {}", e);
+    }else if(in_filter and !in_hashes){
+      println("False positive: {}", e);
+    }else{
+      println("True negative: {}", e);
+    } 
+  }
+}
+
+int main(){
+
+  check(); 
+
 }
